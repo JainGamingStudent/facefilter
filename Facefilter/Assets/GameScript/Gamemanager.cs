78a815666611a16c6dc17e778c6989382741f1cc
Prabakar <58321069+prabakar-s@users.noreply.github.com>
2020-03-26 13:10:02 +0530

    first update
    update ar foundation face filter

---
+++ Facefilter/Assets/GameScript/Gamemanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("id", i);
+    }
+
+
+    public void nextimage()
+    {
+        i++;
+        if(i>3)
+        {
+            i = 0;
+        }
+        PlayerPrefs.SetInt("id", i);
+    }
+ 
+}
---
+++ Facefilter/Assets/GameScript/TextureChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChanger : MonoBehaviour
+{
+    public Texture[] alltexture;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Renderer>().material.mainTexture = alltexture[PlayerPrefs.GetInt("id")];
+    }
+}
